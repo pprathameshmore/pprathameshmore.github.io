3fc5b8a9a9c379ce3182dc00087acfa6f9176b23
Prathamesh More <moreprathameshmore@hotmail.com>
2020-04-07 09:25:57 +0530

    Update resume and API

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = getQuote;
 
 function getQuote() {
 
-    fetch('https://quote-garden.herokuapp.com/quotes/random').then(res => res.json())
+    fetch('https://quote-garden.herokuapp.com/api/v2/quotes/random').then(res => res.json())
         .then(quote => {
             document.getElementById('quote').innerHTML = "<p>" + quote.quoteText + "</p><p>" + quote.quoteAuthor + "</p>";
         });
