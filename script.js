3276c9bba063fc5f0a28117cead3ba19c8a0d73e
Prathamesh More <moreprathameshmore@hotmail.com>
2020-04-07 16:02:40 +0530

    fix quote

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function getQuote() {
 
     fetch('https://quote-garden.herokuapp.com/api/v2/quotes/random').then(res => res.json())
         .then(quote => {
-            document.getElementById('quote').innerHTML = "<p>" + quote.quoteText + "</p><p>" + quote.quoteAuthor + "</p>";
+            document.getElementById('quote').innerHTML = "<p>" + quote.quote.quoteText + "</p><p>" + quote.quote.quoteAuthor + "</p>";
         });
 }
